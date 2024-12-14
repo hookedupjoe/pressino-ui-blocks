e5e46099b6f56054ec023ced411396309b425c41
Joseph Francis <hookedupjoe@users.noreply.github.com>
2024-12-13 22:15:19 -0500

    Version Bump to Production 1

--- pressino-ui-blocks.php
+++ pressino-ui-blocks.php
@@ -4,7 +4,7 @@
  * Description:       Semantic UI styled block controls
  * Requires at least: 6.6
  * Requires PHP:      7.2
- * Version:           0.1.0
+ * Version:           1.0.1
  * Author:            W Joseph Francis
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
