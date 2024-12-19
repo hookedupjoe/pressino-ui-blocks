6933fd39c1ee8318ec20f7131a9ee33b60faa9f2
Joseph Francis <hookedupjoe@users.noreply.github.com>
2024-12-18 19:08:51 -0500

    wrap blocprops around entire respoinse for button

--- src/blocks/pressino-ui-button/edit.js
+++ src/blocks/pressino-ui-button/edit.js
@@ -21,42 +21,42 @@ export default function Edit(theProps) {
     const { url, useicon } = attributes;
     const [isQuickInserterOpen, setQuickInserterOpen] = useState(false);
     const [isInserterOpen, setInserterOpen] = useState(false);
-    
+
 
     return <>
-
-        <InspectorControls>
-
-            <PanelBody title={istr('General Settings')}>
-                {PressinoUI.getStandardProperty(theProps, 'text', 'Button Text', 'text')}
-                {PressinoUI.getStandardProperty(theProps, 'url', 'Target Content or Link', 'url')}
-                {(url && PressinoUI.getStandardProperty(theProps, 'urlopentab', 'Open URL in new tab?', 'checkbox'))}
-                {PressinoUI.getStandardProperty(theProps, 'color', 'Button Color', 'colors')}
-                {PressinoUI.getStandardProperty(theProps, 'size', 'Size', 'sizes')}
-                {PressinoUI.getStandardProperty(theProps, 'circular', 'Circular', 'checkbox')}
-                {PressinoUI.getStandardProperty(theProps, 'basic', 'Outlined', 'checkbox')}
-                {PressinoUI.getStandardProperty(theProps, 'compact', 'Compact', 'checkbox')}
-                {PressinoUI.getStandardProperty(theProps, 'fluid', 'Full Width', 'checkbox')}
-                {PressinoUI.getStandardProperty(theProps, 'attached', 'Attached', 'attached')}
-                {PressinoUI.getStandardProperty(theProps, 'useicon', 'Use Icon', 'checkbox')}
-                {(useicon && PressinoUI.getSettingsForIcon({label: 'Select Icon',attname: 'iconname',isInserterOpen,setInserterOpen,isQuickInserterOpen,setQuickInserterOpen,attributes,setAttributes}))}
-                {/* {PressinoUI.getStandardProperty(theProps, 'iconnamesel', 'Icon', 'icon',false, false,{isQuickInserterOpen, setQuickInserterOpen})} */}
-                {(useicon && PressinoUI.getStandardProperty(theProps, 'iconname', "Icon Name", 'text'))}
-                {(useicon && PressinoUI.getStandardProperty(theProps, 'iconalign', 'Icon on Right Side', 'checkbox'))}
-                {(useicon && PressinoUI.getStandardProperty(theProps, 'iconaslabel', 'Icon as Label', 'checkbox'))}
-            </PanelBody>
-
-
-            <PanelBody title={istr('Formatting Options')}>
-                {PressinoUI.getStandardProperty(theProps, 'float', 'Float Left/Right', 'float')}
-                {PressinoUI.getStandardProperty(theProps, 'margin', 'Margin', 'margin')}
-                {PressinoUI.getStandardProperty(theProps, 'padding', 'Padding', 'padding')}
-                {PressinoUI.getStandardProperty(theProps, 'bottommargin', 'Bottom Margin', 'bottommargin')}
-                {PressinoUI.getStandardProperty(theProps, 'classes', "Additional CSS Class(es)", 'text')}
-            </PanelBody>
-
-        </InspectorControls>
         <div {...blockProps}>
+            <InspectorControls>
+
+                <PanelBody title={istr('General Settings')}>
+                    {PressinoUI.getStandardProperty(theProps, 'text', 'Button Text', 'text')}
+                    {PressinoUI.getStandardProperty(theProps, 'url', 'Target Content or Link', 'url')}
+                    {(url && PressinoUI.getStandardProperty(theProps, 'urlopentab', 'Open URL in new tab?', 'checkbox'))}
+                    {PressinoUI.getStandardProperty(theProps, 'color', 'Button Color', 'colors')}
+                    {PressinoUI.getStandardProperty(theProps, 'size', 'Size', 'sizes')}
+                    {PressinoUI.getStandardProperty(theProps, 'circular', 'Circular', 'checkbox')}
+                    {PressinoUI.getStandardProperty(theProps, 'basic', 'Outlined', 'checkbox')}
+                    {PressinoUI.getStandardProperty(theProps, 'compact', 'Compact', 'checkbox')}
+                    {PressinoUI.getStandardProperty(theProps, 'fluid', 'Full Width', 'checkbox')}
+                    {PressinoUI.getStandardProperty(theProps, 'attached', 'Attached', 'attached')}
+                    {PressinoUI.getStandardProperty(theProps, 'useicon', 'Use Icon', 'checkbox')}
+                    {(useicon && PressinoUI.getSettingsForIcon({ label: 'Select Icon', attname: 'iconname', isInserterOpen, setInserterOpen, isQuickInserterOpen, setQuickInserterOpen, attributes, setAttributes }))}
+                    {/* {PressinoUI.getStandardProperty(theProps, 'iconnamesel', 'Icon', 'icon',false, false,{isQuickInserterOpen, setQuickInserterOpen})} */}
+                    {(useicon && PressinoUI.getStandardProperty(theProps, 'iconname', "Icon Name", 'text'))}
+                    {(useicon && PressinoUI.getStandardProperty(theProps, 'iconalign', 'Icon on Right Side', 'checkbox'))}
+                    {(useicon && PressinoUI.getStandardProperty(theProps, 'iconaslabel', 'Icon as Label', 'checkbox'))}
+                </PanelBody>
+
+
+                <PanelBody title={istr('Formatting Options')}>
+                    {PressinoUI.getStandardProperty(theProps, 'float', 'Float Left/Right', 'float')}
+                    {PressinoUI.getStandardProperty(theProps, 'margin', 'Margin', 'margin')}
+                    {PressinoUI.getStandardProperty(theProps, 'padding', 'Padding', 'padding')}
+                    {PressinoUI.getStandardProperty(theProps, 'bottommargin', 'Bottom Margin', 'bottommargin')}
+                    {PressinoUI.getStandardProperty(theProps, 'classes', "Additional CSS Class(es)", 'text')}
+                </PanelBody>
+
+            </InspectorControls>
+
             {tmpDisplay}
         </div>
     </>
