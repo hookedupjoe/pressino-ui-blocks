50d2de7adebea298cd06863514a49e019a322e4c
Joseph Francis <hookedupjoe@users.noreply.github.com>
2024-12-21 22:16:00 -0500

    Use text variable

--- src/blocks/pressino-ui-richtext/save.js
+++ src/blocks/pressino-ui-richtext/save.js
@@ -8,7 +8,7 @@ import { RichText } from '@wordpress/block-editor';
 export default function save( props ) {
     const blockProps = useBlockProps.save();
     const {attributes} = props;
-    return <RichText.Content className={'ui header large ' + attributes.color || ''} tagName="h2" value={attributes.content} />;
+    return <RichText.Content className={'ui header large ' + attributes.color || ''} tagName="h2" value={attributes.text} />;
 
     // var tmpDisplay = display({props, attributes: props.attributes, editMode: false});
     // return tmpDisplay;
