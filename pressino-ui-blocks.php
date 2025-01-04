3aeddf0446990a224d21c66d8a0d168fb9c4097f
Joseph Francis <hookedupjoe@users.noreply.github.com>
2025-01-04 13:06:16 -0500

    bump to 2.0.8a

--- pressino-ui-blocks.php
+++ pressino-ui-blocks.php
@@ -4,7 +4,7 @@
  * Description:       Semantic UI styled block controls
  * Requires at least: 6.6
  * Requires PHP:      7.2
- * Version:           2.0.8
+ * Version:           2.0.8a
  * Author:            W Joseph Francis
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
@@ -16,7 +16,7 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; 
 }
-define( 'PRESSINO_UI_BLOCKS_CORE_VERSION', '2.0.8' );
+define( 'PRESSINO_UI_BLOCKS_CORE_VERSION', '2.0.8a' );
 
 
 define( 'PRESSINO_UI_BLOCKS_FILE', __FILE__ );
